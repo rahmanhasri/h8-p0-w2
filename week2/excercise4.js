d1c46fe8d5ad3b1a98599e4dbbd28f2effa33263
gingermilk <cingcangkelyng@gmail.com>
2018-10-22 23:32:40 +0700

    tugas hari senen dan selasa

---
+++ week2/excercise4.js
@@ -0,0 +1,42 @@
+var tanggal = 25; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 2; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 2119; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+switch (bulan) {
+  case 1:
+    console.log(`${tanggal} Januari ${tahun}`)
+    break;
+  case 2:
+    console.log(`${tanggal} Februari ${tahun}`)
+    break;
+  case 3:
+    console.log(`${tanggal} Maret ${tahun}`)
+    break;
+  case 4:
+    console.log(`${tanggal} April ${tahun}`)
+    break;
+  case 5:
+    console.log(`${tanggal} Mei ${tahun}`)
+    break;
+  case 6:
+    console.log(`${tanggal} Juni ${tahun}`)
+    break;
+  case 7:
+    console.log(`${tanggal} Juli ${tahun}`)
+    break;
+  case 8:
+    console.log(`${tanggal} Agustus ${tahun}`)
+    break;
+  case 9:
+    console.log(`${tanggal} September ${tahun}`)
+    break;
+  case 10:
+    console.log(`${tanggal} Oktober ${tahun}`)
+    break;
+  case 11:
+    console.log(`${tanggal} November ${tahun}`)
+    break;
+  default:
+    console.log(`${tanggal} Desember ${tahun}`)
+    break;
+}
