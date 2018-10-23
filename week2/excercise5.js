a9ea9e6d6ea87e3958004873cfbde47093f104eb
gingermilk <cingcangkelyng@gmail.com>
2018-10-23 23:32:51 +0700

    yha

---
+++ week2/excercise5.js
@@ -0,0 +1,58 @@
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+//jawaban 1
+console.log(word.concat(' ',second,' ',third,' ',fourth,' ',fifth,' ',sixth,' ',seventh));
+
+//jawaban 2
+var word = 'wow JavaScript is so cool';
+var FirstWord = word[0] + word[1] + word[2];
+var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; // do your own!
+var thirdWord = word[15] + word[16];
+var fourthWord = word[18] + word[19];
+var fifthWord = word[21] + word[22] + word[23] + word[24];
+
+
+//soal
+console.log('First Word: ' + FirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+//jawaban 3
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4,14);
+var thirdWord3 = word.substring(15,17);
+var fourthWord3 = word.substring(18,20);
+var fifthWord3 = word.substring(21,25);
+
+//soal
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+
+//jawaban 4
+
+var word4 = 'wow JavaScript is so cool';
+var firstWord4 = word.substring(0, 3);
+var secondWord4 = word.substring(4,14);
+var thirdWord4 = word.substring(15,17);
+var fourthWord4 = word.substring(18,20);
+var fifthWord4 = word.substring(21,25);
+
+
+
+console.log('First Word: ' + firstWord4 + ', with length: ' + firstWord4.length);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWord4.length);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWord4.length);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWord4.length);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWord4.length);
---
+++ week2/exercise6.js
@@ -0,0 +1,20 @@
+var i = 0;
+var j = 20;
+
+while(i<=20) {
+  if(i===0){
+    console.log('LOOPING PERTAMA');
+  } else {
+    console.log(i + ' I love coding')
+  }
+  i+=2;
+}
+
+while(j>0) {
+  if(j===20){
+    console.log('LOOPING KEDUA' + '\n' + j + ' I will become fullstack developer')
+  } else {
+    console.log(j + ' I will become fullstack developer')
+  }
+  j-=2;
+}
