d8dfe8d6f093c6a88e10e83450861a27097ac5f2
rahmanhasri <rahman.hasri@gmail.com>
2018-11-21 21:51:27 +0700

    refactor melee

--- week5/exercise6.js
+++ week5/exercise6.js
@@ -1,43 +1,59 @@
-function meleeRangedGrouping (str) {
-  //your code here
-  if (str.length === 0){
-    return []
-  }
-  let string = ''
-  let obj = {}
-  let array = []
-  for (let i = 0 ; i<str.length ; i++){
-    if (str[i] !== ','){
-      string += str[i]
-    } else {
-      if (string.length > 0){
-        array.push(string)
-        string = ''
-      }
-    }
-    if (i === str.length-1 && string.length > 0){
-      array.push(string)
-    }
-  }
-  // console.log(array)
-  obj.ranged = []
-  obj.melee = []
+// function meleeRangedGrouping (str) {
+//   //your code here
+//   if (str.length === 0){
+//     return []
+//   }
+//   let string = ''
+//   let obj = {}
+//   let array = []
+//   for (let i = 0 ; i<str.length ; i++){
+//     if (str[i] !== ','){
+//       string += str[i]
+//     } else {
+//       if (string.length > 0){
+//         array.push(string)
+//         string = ''
+//       }
+//     }
+//     if (i === str.length-1 && string.length > 0){
+//       array.push(string)
+//     }
+//   }
+//   // console.log(array)
+//   obj.ranged = []
+//   obj.melee = []
+//
+//   for (let i = 0 ; i<array.length ; i++){
+//     string = ''
+//     for (let j = 0 ; j<array[i].length ; j++){
+//       if (array[i][j] !== '-'){
+//         string += array[i][j]
+//       } else if (array[i][j] === '-' && array[i][j+1] === 'R'){
+//         obj.ranged.push(string);
+//         break;
+//       } else if (array[i][j] === '-' && array[i][j+1] === 'M'){
+//         obj.melee.push(string);
+//         break;
+//       }
+//     }
+//   }
+//   return [obj.ranged, obj.melee]
+// }
 
-  for (let i = 0 ; i<array.length ; i++){
-    string = ''
-    for (let j = 0 ; j<array[i].length ; j++){
-      if (array[i][j] !== '-'){
-        string += array[i][j]
-      } else if (array[i][j] === '-' && array[i][j+1] === 'R'){
-        obj.ranged.push(string);
-        break;
-      } else if (array[i][j] === '-' && array[i][j+1] === 'M'){
-        obj.melee.push(string);
-        break;
+function meleeRangedGrouping(str) {
+    let splitHero = str.split(',')
+    let rangedHero = []
+    let meleeHero = []
+
+    for(let i = 0 ; i<splitHero.length ; i++){
+      if (splitHero[i].toLowerCase().indexOf('ranged') !== -1){
+        rangedHero.push(splitHero[i].split('-')[0])
+      } else if (splitHero[i].toLowerCase().indexOf('melee') !== -1){
+        meleeHero.push(splitHero[i].split('-')[0])
       }
     }
-  }
-  return [obj.ranged, obj.melee]
+    // console.log(rangedHero, meleeHero)
+    return [ rangedHero, meleeHero ]
 }
 
 // TEST CASE
